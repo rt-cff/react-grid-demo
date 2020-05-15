34ba5b01f1961171f60071b0909372fcd056d7f1
Raymond Tam <raymondtamtwtp@gmail.com>
2020-05-15 17:50:42 +0800

    publish: modify homepage attribute of package.json

---
+++ precache-manifest.00c7c21010bfffdc82ff6a538f544eda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55362f9fd4b2738d90122e1dae0dbe3",
+    "url": "/react-form-demo/index.html"
+  },
+  {
+    "revision": "037174f6907813113496",
+    "url": "/react-form-demo/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "06586706dc34030ffe80",
+    "url": "/react-form-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "037174f6907813113496",
+    "url": "/react-form-demo/static/js/2.23a97b54.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-form-demo/static/js/2.23a97b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06586706dc34030ffe80",
+    "url": "/react-form-demo/static/js/main.47aae410.chunk.js"
+  },
+  {
+    "revision": "c5edc7a2854b34211c67",
+    "url": "/react-form-demo/static/js/runtime-main.de6daefc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60e29a0baa342c8e9f1fd08ae11b81e2.js"
+  "/react-form-demo/precache-manifest.00c7c21010bfffdc82ff6a538f544eda.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-form-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
