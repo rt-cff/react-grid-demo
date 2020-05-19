9cadbc3b69461695e2a4211af50764dc90233311
Raymond Tam <raymondtamtwtp@gmail.com>
2020-05-19 16:32:28 +0800

    publish: implement grid with virtualizationa and mask with hook

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-form-demo/precache-manifest.1968447161f8ef3ed919da7dda95b904.js"
+  "/precache-manifest.d2d668abc8392ba24d65c284262fda50.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-form-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
