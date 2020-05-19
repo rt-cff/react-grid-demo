15f84843b2c29a233d2bbd9b26f1fa0016af11d8
Raymond Tam <raymondtamtwtp@gmail.com>
2020-05-19 16:44:59 +0800

    publish: modify package.json

---
+++ precache-manifest.d98b8152bb905fd828086b910241340a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0b91220f8d6910ecf5d10c104d476e",
+    "url": "/react-gird-demo/index.html"
+  },
+  {
+    "revision": "0ee3f72a6c56655dc3d1",
+    "url": "/react-gird-demo/static/css/main.70248e91.chunk.css"
+  },
+  {
+    "revision": "e1d89bc2db0524a16f93",
+    "url": "/react-gird-demo/static/js/2.3df28a8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gird-demo/static/js/2.3df28a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee3f72a6c56655dc3d1",
+    "url": "/react-gird-demo/static/js/main.06f1ad7b.chunk.js"
+  },
+  {
+    "revision": "9c7d5a4833291ba6b6fe",
+    "url": "/react-gird-demo/static/js/runtime-main.b9a409f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2d668abc8392ba24d65c284262fda50.js"
+  "/react-gird-demo/precache-manifest.d98b8152bb905fd828086b910241340a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gird-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
